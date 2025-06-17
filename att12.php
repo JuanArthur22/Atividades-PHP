ec5ce53bf69d0436cf1e34ebffa5f7feaefadabd
Juan Arthur <juanarthursenai.123@gmail.com>
2025-06-17 11:04:40 -0300

    Create att12.php

---
+++ att12.php
@@ -0,0 +1,10 @@
+<?php
+  $usuario = [
+    "nome"=> "Juan",
+    "idade"=> 17,
+    "email"=> "juan@gmail.com",
+];
+echo "Nome: " . $usuario["nome"] . '<br>';
+echo 'Idade: ' . $usuario['idade'] . '<br>';
+echo 'Email: ' . $usuario['email'];
+?>
